b1d1fbc2d1e9dc870619938b6ae3b73f0b686c5e
David Malison <djtmalison@gmail.com>
2018-03-08 20:40:14 -0600

    Add small changes to check_sim.R

--- check_sim.R
+++ check_sim.R
@@ -9,12 +9,7 @@ load('~/bin/JMP/work/sim')
 k = 1
 i = parNames[k]
 regex = grep(paste(paste(i,"[[].*[]]", sep = ""),"|","^",i,"$", sep = ""), names(fit_stan), value = T)
-
 cbind(as.vector(t(parTrue[[i]])), summary(fit_stan, pars = i, use_cache = F)[[1]][,c(1,6,4,8,9,10)])
 (t(parTrue[[i]]) >= summary(fit_stan, pars = i, use_cache = F)[[1]][,4])*(t(parTrue[[i]]) <= summary(fit_stan, pars = i, use_cache = F)[[1]][,8])
 traceplot(fit_stan, pars = regex[sort(sample(1:length(regex), min(length(regex),9)))], inc_warmup = T)
 k = k + 1
-
-theta_1 <- extract(fit_stan, pars = "theta_1")[[1]]
-
-traceplot(fit_stan, pars = "corr_1")
